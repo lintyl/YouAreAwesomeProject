498dc0b7683c41c4bc316cd015a576cd427f6530
Tyler Lin <lintyl@bc.edu>
2025-09-04 17:48:39 -0400

    Ch1.9

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -11,31 +11,39 @@ struct ContentView: View {
     @State private var imageName = ""
     @State private var imageNumber = 0
     @State private var message = ""
+    @State private var messageNumber = 0
     var body: some View {
         VStack {
             Spacer()
+            
             Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
+            
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+            
             Spacer()
+            
             Button("Show Message") {
-                let message1 = "You Are Awesome!"
-                let message2 = "You Are Great!"
-//                if message == message1 {
-//                    message = message2
-//                    imageName = string1
-//                } else {
-//                    message = message1
-//                    imageName = string2
-//                }
-                message = (message == message1 ? message2 : message1)
-//                imageName = (imageName == "image0" ? "image1" : "image0")
+                let messages = ["You Can Do Better...",
+                                "That's Very Mediocre!",
+                                "You Kinda Don't Suck!",
+                                "You Did An Okay Job?",
+                                "Your Mom May Or May Not Approve!",
+                                "Prof G Tossed You A Snickers Bar, But You're Deathly Allergic to Nuts!!!"]
+                
+                message = messages[messageNumber]
+                messageNumber += 1
+                if messageNumber == messages.count {
+                    messageNumber = 0
+                }
+                
                 imageName = "image\(imageNumber)"
                 imageNumber += 1
                 if imageNumber > 9 {
