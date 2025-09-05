a18a5fc3d85fe8fe0321b756232f382ec9865cb2
Tyler Lin <lintyl@bc.edu>
2025-09-05 08:46:23 -0400

    Ch1.12

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,10 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var imageName = ""
-    @State private var imageNumber = 0
     @State private var message = ""
-    @State private var messageNumber = 0
+    @State private var imageName = ""
+    @State private var lastMessageNumber = -1
+    @State private var lastImageNumber = -1
+    
+    
     var body: some View {
         VStack {
             Text(message)
@@ -41,8 +43,19 @@ struct ContentView: View {
                                 "Your Mom May Or May Not Approve!",
                                 "Prof G Tossed You A Snickers Bar, But You're Deathly Allergic to Nuts!!!"]
                 
-                message = messages[Int.random(in: 0...messages.count-1)]
-                imageName = "image\(Int.random(in: 0...9))"
+                var messageNumber = Int.random(in: 0...messages.count-1)
+                while messageNumber == lastMessageNumber {
+                    messageNumber = Int.random(in: 0...messages.count-1)
+                }
+                message = messages[messageNumber]
+                lastMessageNumber = messageNumber
+                
+                var imageNumber = Int.random(in: 0...9)
+                while imageNumber == lastImageNumber {
+                    imageNumber = Int.random(in: 0...9)
+                }
+                imageName = "image\(imageNumber)"
+                lastImageNumber = imageNumber
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
